a24952326260ff128b28f70b7d3b77918dbec224
Romario <rom.nosov2014@yandex.ru>
2023-02-08 01:23:44 +0300

    add first code to script.js 08/02/2023

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Test #2")
+
+const name = 'Roman'
+const surname = 'Nosov'
+let age = 2023-1997
+let work_period = 11 % 3 + 0.7
+let sport_games = ["tennis", "basketball", "football", "hockey"]
+let result_string = `Hi, my name’s ${surname} ${name}. I’m from Russia. 
+I’m ${age} years old. I work at Russian Bank about ${work_period} years. I’m a analyst. My favorite sport is ${sport_games[1]}.
+There are four people in my family. In my free time, I also like watching YouTube channels about new IT technology.
+My favorite day of the week is Wednesday because it is the day when everybody feel very productive.
+My favorite month is May because spring is already there and the weather is so warm.
+My favorite place is Santorini. It is an island in Greece. I like it because of white buildings 
+with blue roofs and old mill covering this island. There is a black sand also on the beach. 
+The water in Mediterranean Sea is always about 25 degrees.
+I like travelling. I have been to Spain, Turkey, Germany, Finland, Latvia, Denmark, Greece, Tunisia and Cuba.`
+
+
+console.log(result_string)
