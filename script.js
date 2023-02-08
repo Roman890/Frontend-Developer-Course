9db20251c09074f688b1fa5bdfa743178f295c04
Romario <rom.nosov2014@yandex.ru>
2023-02-09 00:12:37 +0300

    add new variables in text for homework 5

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+// declare variables
+const name = 'Roman';
+let age = 2023 - 1997;
+let workPeriod = 11 % 3 + 0.7;
+let familyCount = 'four';
+let sportGames = ["tennis", "basketball", "football", "hockey"];
+let weekDays = ["Sunday", "Monday", "Wednesday"];
+let degrees = 27;
+let isTrue = 4 > 1;
+let countries = ["Spain", "Turkey", "Germany", "Finland", "Latvia", "Denmark", "Greece", "Tunisia", "Cuba"]
 
-const name = 'Roman'
-const surname = 'Nosov'
-let age = 2023-1997
-let work_period = 11 % 3 + 0.7
-let sport_games = ["tennis", "basketball", "football", "hockey"]
-let result_string = `Hi, my name’s ${surname} ${name}. I’m from Russia. 
-I’m ${age} years old. I work at Russian Bank about ${work_period} years. I’m a analyst. My favorite sport is ${sport_games[1]}.
-There are four people in my family. In my free time, I also like watching YouTube channels about new IT technology.
-My favorite day of the week is Wednesday because it is the day when everybody feel very productive.
-My favorite month is May because spring is already there and the weather is so warm.
+// generate of result
+let resultString = `Hi, my name’s ${name}. I’m from Russia. 
+I’m ${age} years old. I work at Russian Bank about ${workPeriod} years. I’m a analyst. My favorite sport is ${sportGames[1]}.
+There are ${familyCount} people in my family. In my free time, I also like watching YouTube channels about new IT technology.
+My favorite day of the week is ${weekDays[2]} because it is the day when everybody feel very productive.
+My favorite month is May because spring is already there and the weather is so warm and temperature about ${degrees++} degrees.
 My favorite place is Santorini. It is an island in Greece. I like it because of white buildings 
-with blue roofs and old mill covering this island. There is a black sand also on the beach. 
-The water in Mediterranean Sea is always about 25 degrees.
-I like travelling. I have been to Spain, Turkey, Germany, Finland, Latvia, Denmark, Greece, Tunisia and Cuba.`
+with blue roofs and old mill covering this island. There is a black sand also on the beach. That is ${isTrue} story.
+The water in Mediterranean Sea is always about ${ 2 + "5"} degrees.
+I like travelling. I have been to ${countries.join(', ')}.`
 
-
-console.log(result_string)
+// output
+console.log(resultString)
